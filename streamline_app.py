71805f873747ee36e12770384bf0a8849ee22e06
mooski529 <youssefmahmoud90@yahoo.com>
2024-09-23 10:18:23 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -47,5 +47,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success("Your Smoothie is ordered!", icon="✅" )
         
-import requests
+
 
